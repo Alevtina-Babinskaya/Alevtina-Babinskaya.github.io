9ac61020dd4949a917f53a72f6f9b454dae74d62
Alevtina-Babinskaya <letters4alka@gmail.com>
2023-09-24 22:02:33 -0800

    My initial version of the task.

---
+++ javascript/main.js
@@ -0,0 +1,69 @@
+const form = document.getElementById('messageToPost');
+const inputName = document.getElementById('fName');
+const inputEmail = document.getElementById('fMail');
+const message = document.getElementById('fMessage'); 
+const ul = document.getElementById('messagePool');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const Name = inputName.value;
+    inputName.value = '';
+    const Email = inputEmail.value;
+    inputEmail.value = '';
+    const Message = message.value;
+    message.value = '';
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    const a = document.createElement('a');
+    const span = document.createElement('span');
+    const removeButton = document.createElement('button');
+    const editButton = document.createElement('button');
+     //   removeButton.id = Name;
+    ul.appendChild(li);
+    li.appendChild(p);
+    p.appendChild(a);
+    a.href = `mailto:${Email}`;
+    a.innerText = `${Name}: `;
+    p.appendChild(span);
+    span.className = "post";
+    span.textContent = Message;
+    li.appendChild(editButton);
+    editButton.value = 'Edit';
+    editButton.textContent = 'Edit';
+    editButton.className = 'editBtn';
+    li.appendChild(removeButton);
+    removeButton.value = 'Remove';
+    removeButton.textContent = 'Remove';
+    removeButton.className = 'removeBtn';
+    
+    
+});
+ul.addEventListener('click', (e) => {
+    const button = e.target;
+    const li = button.parentNode;
+    const p = li.firstElementChild;
+    if (button.textContent === 'Remove') {
+        ul.removeChild(li);
+    }
+    else if (button.textContent === 'Edit') {
+       //const span = p.getElementsByTagName(span);
+       //console.log (span.textContent);
+       const input = document.createElement('input');
+       const span = p.lastElementChild;
+       input.value = span.textContent;
+       p.removeChild(span);
+       p.appendChild(input);
+       button.textContent = 'Save';
+
+    }
+    else if (button.textContent === 'Save') {
+        const span = document.createElement('span');
+        const input = p.lastElementChild;
+        span.textContent = input.value;
+        p.removeChild(input);
+        p.appendChild(span);
+        button.textContent = 'Edit';
+    }
+
+});
+
